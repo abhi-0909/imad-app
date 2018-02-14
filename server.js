515b73e42506cf00776e48468c5332fb22afbf09
abhi-0909 <36410942+abhi-0909@users.noreply.github.com>
2018-02-14 12:11:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two',function (req , res){res.sendFile(path.join(__dirname, 'u
     
 });
 
-app.get('/article-three',function (req , res){res.send('Article three requested and will be served here');
+app.get('/article-three',function (req , res){res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
     
 });
 
