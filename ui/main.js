c72562634e4fa9759173785ab8477b82ff96c334
abhi-0909 <36410942+abhi-0909@users.noreply.github.com>
2018-02-16 20:58:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,12 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'newvalue';
+
+//move 
+var img = document.getElementById('madi');
+img.onclick = function()
+{
+    
+    img.style.marginLeft = '100px';
+    
+};
