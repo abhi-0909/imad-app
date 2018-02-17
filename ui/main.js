928a34660ecca8ceaca4b99a62cc8d4669b6ab2f
abhi-0909 <36410942+abhi-0909@users.noreply.github.com>
2018-02-17 11:12:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,10 @@ button.onclick = function (){
   
   //capture the response and store it in a variable
   request.onreadystatechange = function(){
-      if(request.readyState == XMLHttpRequest.DONE){
-          if(request.status == 200){
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
               var counter = request.responseText;
-              var span = document.getElementById('count');
+              var span = document.getElementById('counter');
               span.innerHTML = counter.toString();
           }
       }
