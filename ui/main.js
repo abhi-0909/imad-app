c6322ddc92f72e9f5a08588e33cd4d3f01cfb674
abhi-0909 <36410942+abhi-0909@users.noreply.github.com>
2018-02-17 10:50:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,13 @@
-console.log('Loaded!');
-//change text
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'newvalue';
-
-//move 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function()
-{
-    var interval = setInterval(moveRight, 100);
-    
+//counter code
+var button = document.getElementById("counter");
+button.onclick = function (){
+  //Make a request to counter endpoint
+  
+  
+  //capture the response and store it in a variable
+  
+  //Render the variable in the correct span
+  counter = counter +1;
+  var span = document.getElementById("count");
+  span.innerHTML = counter.toString();
 };
